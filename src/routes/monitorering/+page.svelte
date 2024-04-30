ebaf3a8ddf9455738c6a9fefe423eada56a20ada
Alban <alban-458@live.dk>
2024-04-30 23:29:31 +0200

    html,css & database implemented

---
+++ src/routes/monitorering/+page.svelte
@@ -0,0 +1,23 @@
+<style>
+    @import './monitorering.css';
+</style>
+
+<div class="container">
+    <div id="graf-vindue" class="graph-container">
+        <!-- Placeholder til graf -->
+        <div id="graf" class="graph">
+            <!-- Graf vil blive indsat her -->
+        </div>
+    </div>
+
+    <div class="button-container">
+        <button class="button" onclick="hentData()">Hent Data</button>
+        <button on:click={refreshGraph}>Genindlæs side</button>
+    </div>
+</div>
+
+<script>
+    function refreshGraph() {
+        location.reload()
+    }
+</script>
